a4df8538e955e140a9f545783c7bddbbeafa75f5
elhamamini <elhamfarvid@gmail.com>
2021-09-25 19:16:47 -0400

    Deliverable3

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -17,3 +17,17 @@ head(total_summary)
 
 lot_summary <- Mecha_table %>%  group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI) , Variance=var(PSI), SD=sd(PSI) ,.groups="keep")
 head(lot_summary)
+
+#Deliverable 3
+
+sample_Mecha <- sample_n(Mecha_table, 50)
+t.test(sample_Mecha$PSI, mu=mean(Mecha_table$PSI))
+
+sample_Mecha_lot1 <- subset(Mecha_table,Manufacturing_Lot=="Lot1" )
+t.test(sample_Mecha_lot1$PSI, mu=mean(Mecha_table$PSI))
+
+sample_Mecha_lot2 <- subset(Mecha_table,Manufacturing_Lot=="Lot2" )
+t.test(sample_Mecha_lot2$PSI, mu=mean(Mecha_table$PSI))
+
+sample_Mecha_lot3 <- subset(Mecha_table,Manufacturing_Lot=="Lot3" )
+t.test(sample_Mecha_lot3$PSI, mu=mean(Mecha_table$PSI))
